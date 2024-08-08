3f48392dff3afd4fa1964d58e5f8f40d922b6fc8
KdntNinja <KdntNinja-2010@proton.me>
2024-08-08 11:43:31 +0100

    First Commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+fn redirect_to_original_url(shortened_url: &str) -> String {
+    return shortened_url.to_string();
+}
+
+fn handle_url_shortening_request(url: &str) -> String {
+    return url.to_string();
+}
+
+fn store_url_mapping(shortened_url: &str, original_url: &str) -> bool {
+    return true;
+}
+
 fn main() {
     println!("Hello, world!");
 }
